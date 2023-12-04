2f5b6e00c4aabc2f7b45374047bed13209f69aba
Anthony <anthony201188@hotmail.co.uk>
2023-12-04 22:14:22 +0100

    Inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# my_package/subpackage/module_b.py
+import numpy as np
+import my_package
+
+def greet_module_b():
+    return "Greetings from Module B!"
+
+def perform_calculations():
+    numbers = np.array([1, 2, 3, 4, 5])
+    sum_numbers = np.sum(_____)
+    average = np.___(_____) # research the numpy method of the average
+    return sum_numbers, average
